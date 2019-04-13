9445f0082467b902f1c3a7603768bd1c90d491a2
renatolaranja <matthew.b.geiger@gmail.com>
2019-04-13 13:30:11 -0400

    added home page component and set up routes

--- ticket-system/src/App.js
+++ ticket-system/src/App.js
@@ -1,72 +1,29 @@
 
-import React, { Component } from 'react'
-import { HashRouter, Route, Link } from "react-router-dom"
-//import logo from './logo.svg'
-import './App.css'
+import React, { Component } from 'react';
+import { HashRouter, BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+//import logo from './logo.svg';
+import './App.css';
+
+import Home from "./component/Home";
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-      <TopWrapper />
-      <InputGroup />
-      </div>
-     
-    )
-   }
-}
+      <Router>
+        <Route path={"Home"} component={Home}/>
+        <Route path={"/"} component={Home}/>
 
-class TopWrapper extends React.Component {
-    render() {
-        return(
+        
+      </Router>
 
-    <div className="card card-login mx-auto text-center bg-dark">
-
-    <div className="card-header mx-auto bg-dark">
-    </div>
-    <div className="container">
-        <h1>IT Help Desk</h1>
-        <p>Please Login to Access Your Tickets</p>
-    </div>
-
-  </div>
-
-  
-
-  
- 
-
-
-        )
-    }
-}
-
-class InputGroup extends Component {
-    render() {
-      return(
-        <form action="/api/login" method = "post">
-        <div className="input-group">
-            <div className="input-group-prepend">
-              <span className="input-group-text"><i className="fas fa-user"></i></span>
-            </div>
-            <input type="text" placeholder="Username" name="username" id="username" required/>
-        </div>
-        <div className="input-group">
-            <div className="input-group-prepend">
-              <span className="input-group-text"><i className="fas fa-key"></i></span>
-            </div>
-            <input type="password" placeholder="Password" name="password" required/>
-        </div>
-          <button id="login">Login</button>
-      </form>
-
-      )
+      );
     }
 
 
 }
 
-const Home = () => <div><h2>Home</h2></div>
+//const Home = () => <div><h2>Home</h2></div>
 const About = () => <div><h2>About</h2></div>
 
 export default App;
