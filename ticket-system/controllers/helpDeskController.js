1c87aec3ae35b0c86df0820b0fec7388af4f348c
schnibb <schnibbend@gmail.com>
2019-04-18 21:35:33 -0400

    completed routings, updated models

--- ticket-system/controllers/helpDeskController.js
+++ ticket-system/controllers/helpDeskController.js
@@ -1,21 +1,55 @@
 const db = require("../models");
 
 module.exports = {
-
     findAll: function(req, res) {
-        db.Helpdesk
-            .find(req.query)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
+        db.Helpdesk.findAll({}).then(function(dbHelpdesk) {
+            res.json(dbHelpdesk);
+        });
     },
+
     create: function(req, res) {
-        db.Helpdesk
-            .create(req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
+        db.Helpdesk.create(req.body).then(function(dbHelpdesk) {
+            res.json(dbHelpdesk);
+        });
     },
+
     update: function(req, res) {
-        db.Helpdesk
-            .findOneAndUpdate({ _ })
+        db.Helpdesk.update(req.body, {
+            where: {
+                id: req.body.id
+            }
+        }).then(function(dbHelpdesk) {
+            res.json(dbHelpdesk);
+        });
+    },
+
+    getOneUser: function(req, res) {
+        db.Users.findOne({
+            where: {
+                username: req.params.username
+            }
+        }).then(function(dbHelpdesk) {
+            res.json(dbHelpdesk);
+        });
+    },
+
+    findById: function(req, res) {
+        db.Helpdesk.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbHelpdesk) {
+            res.json(dbHelpdesk);
+        });
+    },
+
+    getAllUserTickets: function(req, res) {
+        db.Helpdesk.findAll({
+            where: {
+                Created_By: req.params.Created_By
+            }
+        }).then(function(dbHelpdesk) {
+            res.json(dbHelpdesk);
+        });
     }
-}
+};
--- ticket-system/routes/api/helpdesk.js
+++ ticket-system/routes/api/helpdesk.js
@@ -1,16 +1,19 @@
 const router = require("express").Router();
 const helpDeskController = require("../../controllers/helpDeskController");
 
-// Matches with "/api/books"
-router.route("/tickets")
+router
+    .route("/tickets")
     .get(helpDeskController.findAll)
     .post(helpDeskController.create);
 
-// Matches with "/api/books/:id"
 router
     .route("/:id")
     .get(helpDeskController.findById)
     .put(helpDeskController.update)
-    .delete(helpDeskController.remove);
+
+
+router
+    .route("/usersTickets")
+    .get(helpDeskController.getOneUser);
 
 module.exports = router;
