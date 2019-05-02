741d244a5ec7d46ba7faa828a6dc516a52349cb6
Dolson <D.olson4295@yahoo.com>
2019-05-02 19:11:30 -0400

    Login Changes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,11 @@ import axios from "axios";
 
 export default {
     getTickets: function() {
-        console.log("axios called");
         return axios.get("/api/tickets");
+    },
+
+    getLogin: function()
+    {
+        return axios.post("/api/login");
     }
 };
