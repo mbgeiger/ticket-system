4c0ecf244e9e5398bfa4a460734865572e4be2bb
renatolaranja <matthew.b.geiger@gmail.com>
2019-05-03 22:05:47 -0400

    ux fixes

--- client/src/component/Signup.js
+++ client/src/component/Signup.js
@@ -1,10 +1,16 @@
-import React, {Component} from 'react';
+
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import axios from 'axios';
+
+
 
 class Signup extends Component {
     constructor(props) {
         super(props);
-  
-  
         this.state = {
             username: '',
             password: ''
@@ -22,44 +28,112 @@ class Signup extends Component {
     
     render() {
         return (
-            <div className="container">
-            <div className="jumbotron">
-            <div className="signup"><h2>Create an account</h2>
-            
-            <div className="form-inline" >
-                
-            <div className="form group">
-                    <input className="form-control"
-                    type="text"
-                    placeholder="Username"
-                    onChange={event => this.setState({username: event.target.value})}
-                    />
-                    <br/>
-                    <br/>
-                    <input
-                        className="form-control"
-                        type="Password"
-                        placeholder="Password"
-                        onChange={event => this.setState({password: event.target.value})}
-                        />
-                        
-                        <button
-                            className="btn btn-dark"
-                            type="button"
-                            onClick={() => this.signUp()}
-                            >
-                            Sign up
-                            </button>
-                </div>
-                </div>
-                </div>
-                </div>
-                </div>
-            
-           
-
-        )
+            <div>
+            <MuiThemeProvider>
+              <div>
+              <AppBar
+                 title="Register a New User"
+               />
+               <Registeruser />
+              
+              </div>
+             </MuiThemeProvider>
+          </div>
+        );
+      }
     }
-}
+    const style = {
+      margin: 15,
+    };
+
+
+    class Registeruser extends Component {
+        constructor() {
+          super();
+          this.state = {
+            username: '',
+            password: '',
+            error: '',
+            login:''
+          };
+      
+          this.handlePassChange = this.handlePassChange.bind(this);
+          this.handleUserChange = this.handleUserChange.bind(this);
+          this.handleSubmit = this.handleSubmit.bind(this);
+          this.dismissError = this.dismissError.bind(this);
+        }
+      
+        dismissError() {
+          this.setState({ error: '' });
+        }
+      
+        handleSubmit(evt) {
+          evt.preventDefault();
+          var data='';
+          axios.post("/api/login",null,{params:{username:this.state.username, password:this.state.password}})
+                .then((response) => 
+                    {
+                        if(response.data.length < 1)
+                          {
+                            console.log("Incorrect Username or Password")
+                            this.setState({login: false});
+                            console.log(this.state.login);
+                          }
+                          else
+                          {
+                            this.setState({login:true});
+                          }
+                    })
+                .catch(err => console.warn(err));         
+              
+              console.log(data)
+        }
+      
+        handleUserChange(evt) {
+          this.setState({
+            username: evt.target.value,
+          });
+        };
+      
+        handlePassChange(evt) {
+          this.setState({
+            password: evt.target.value,
+          });
+        }
+          render() {
+            if(this.state.login)
+              {
+                document.location.href = "./signup"; 
+              }
+            else
+            {
+              return(
+                  <form  onSubmit={this.handleSubmit}>
+                  <div className="input-group">
+                      <div className="input-group-prepend">
+                        <span className="input-group-text"><i className="fas fa-user"></i></span>
+                      </div>
+                      <input type="text" data-test="username" placeholder="Username" value={this.state.username} onChange={this.handleUserChange} required />
+                  </div>
+                  <div className="input-group">
+                      <div className="input-group-prepend">
+                        <span className="input-group-text"><i className="fas fa-key"></i></span>
+                      </div>
+                      <input type="password" data-test="password" placeholder="Password" value={this.state.password} onChange={this.handlePassChange} required />
+                  </div>
+
+                    <button className="btn btn-dark" id="Signup">Sign Up</button>
+                </form>
+      
+              )
+            }
+          }
+      
+          
+      
+      
+      }
+      
+    
 
 export default Signup
